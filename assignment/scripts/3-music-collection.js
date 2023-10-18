4eff8bf677ee22a123cd3165eba3d5142a349ff3
Braden Soller <braden.soller18@gmail.com>
2023-10-18 15:44:51 -0500

    created function that can add albums to my collections, i also added the 6 albums and logged them

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,13 +1,71 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+function addToCollection(collection, title, artist, yearPublished){
+    let album = {
+      title: title,
+      artist: artist,
+      yearPublished: yearPublished
+    }
+  collection.push(album)
+  return album;
+}
 
 
+let myCollection = [];
+let album1 = {
+  title: "The Life of Pablo",
+  artist: 'Kanye',
+  yearPublished: 2016
+}
+console.log(addToCollection(myCollection, album1.title, album1.artist,album1.yearPublished));
+
+let album2 = {
+  title: 'I am > i was',
+  artist: '21 savage',
+  yearPublished: 2018
+}
+
+ console.log(addToCollection(myCollection, album2.title, album2.artist, album2.yearPublished));
+
+ let album3 = {
+  title: 'Self Titled',
+  artist: 'Zach Bryan',
+  yearPublished:'2023'
+ }
+
+console.log(addToCollection(myCollection, album3.title, album3.artist, album3.yearPublished));
+
+let album4 = {
+  title: 'What Could Possibly Go Wrong',
+  artist: 'Domonic Fike',
+  yearPublished: 2020
+}
+console.log(addToCollection(myCollection, album4.title, album4.artist, album4.yearPublished));
+
+let album5 = {
+  title: 'Dont Forget About Me , Demos',
+  artist: 'Domonic Fike',
+  yearPublished: 2018
+}
+
+console.log(addToCollection(myCollection, album5.title, album5.artist, album5.yearPublished));
+
+let album6 = {
+  title: 'Continuum',
+  artist: 'John Mayor',
+  yearPublished: 2006
+}
+
+console.log(addToCollection(myCollection, album6.title, album6.artist, album6.yearPublished));
 
 
+function showCollection(collection) {
+  
+}
 
-
-
+console.log("My Collection:");
+showCollection(myCollection);
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
